7a9a23931b39703352ebdbbd62cd002336f18df3
Adam Smith <adamps724@gmail.com>
2024-05-27 20:42:07 -0500

    added splide

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from "styled-components";
-
-
+import {Splide, SplideSlide } from "@splidejs/react-splide"
+import '@splidejs/react-splide/css';
 
 function Popular() {
 
@@ -22,21 +22,29 @@ const getPopular = async () => {
 
   return ( 
   <div> 
-    {popular.map(recipe => {
-      return(
+    
           <Wrapper>
             <h3>Popular Picks</h3>
+            <Splide options = {{
+              perPage: 4,
+              arrows: false,
+              pagination: false,
+              drag: 'free',
+              gap: '5rem',
+            }}>
             {popular.map((recipe) => {
               return (
+                <SplideSlide>
                 <Card>
                   <p>{recipe.title}</p>
                   <img src = {recipe.image} alt={recipe.title} />
                 </Card>
+                </SplideSlide>
               )
             })}
+            </Splide>
           </Wrapper>
-          );
-        })}
+         
   </div>
   ) 
 }
@@ -48,6 +56,13 @@ const Wrapper = styled.div`
 const Card = styled.div`
   min-height: 25 rem;
   border-radius: 2rem;
+  overflow: hidden;
+  img {
+    border-radius: 2rem;
+
+
+  }
+  
 `;
 
 export default Popular
