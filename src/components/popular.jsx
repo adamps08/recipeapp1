0465d4d1ea736b2853c38532aec4daee19c5ffc3
Adam Smith <adamps724@gmail.com>
2024-06-15 21:33:12 -0500

    begun adding details page

--- src/components/popular.jsx
+++ src/components/popular.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
+import {Link} from 'react-router-dom'
 
 function Popular() {
   const [popular, setPopular] = useState([]);
@@ -45,10 +46,12 @@ function Popular() {
           {popular.map((recipe) => (
             <SplideSlide key={recipe.id}>
               <Card>
-                <p>{recipe.title}</p>
-                <img src={recipe.image} alt={recipe.title} />
+                <Link to ={"/recipe/" + recipe.id}>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                  <Gradient />
+                </Link>
               </Card>
-              <Gradient />
             </SplideSlide>
           ))}
         </Splide>
